8848808158bb422bc8e4f06f1d75a3dd864cb306
vagrant <vagrant@ubuntu-oneiric.(none)>
2013-05-24 19:00:59 +0100

    Added version to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='dynsettings',
     packages=find_packages(),  
     description = 'Stores key/value settings in database, allowing for live updating of settings.',
     url = 'http://github.com/infoscout/dynsettings',
-    #version = '0.1dev',    
+    version = '1.0',    
     install_requires=[
         'django==1.4',
     ]
