be89e91d773c37783da670964570474b39c57f76
vagrant <vagrant@ubuntu-oneiric.(none)>
2013-05-26 16:39:02 +0100

    Tagging release as 1.0

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from isc_ops.setup_tools import setup, current_version
 
 setup(name='dynsettings',
     packages=find_packages(),  
     description = 'Stores key/value settings in database, allowing for live updating of settings.',
     url = 'http://github.com/infoscout/dynsettings',
-    version = '1.0',    
+    version = current_version(),    
     install_requires=[
         'django==1.4',
     ]
 )
-
