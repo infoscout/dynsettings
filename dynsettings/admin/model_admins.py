575530785528d19874071393944bfc5d17c7c05f
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-03-18 19:28:39 -0700

    CA-778: django.conf.urls.defaults -> django.conf.urls

--- dynsettings/admin/model_admins.py
+++ dynsettings/admin/model_admins.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from django.contrib import admin
 from dynsettings.models import BucketSetting
 from dynsettings.admin.views import edit_settings
