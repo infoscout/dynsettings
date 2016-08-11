359ca64d8b85b7e6f066fe2bf6199db251fa07a8
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-08-11 11:28:01 -0700

    Fix indenting/spacing

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import find_packages, setup
 
+
 with open('VERSION','r') as f:
     version = f.read().strip()
-    
-setup(name='dynsettings',
-    packages=find_packages(),  
+
+
+setup(
+    name='dynsettings',
+    packages=find_packages(),
     include_package_data=True,
-    description = 'Stores key/value settings in database, allowing for live updating of settings.',
-    url = 'http://github.com/infoscout/dynsettings',
-    version = version,    
+    description='Stores key/value settings in database, allowing for live updating of settings.',
+    url='http://github.com/infoscout/dynsettings',
+    version=version,
     install_requires=[
         'django>=1.4',
     ]
