a76025a3ee4c205ae1fb26dc2de01ef4cd09605c
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-02-02 13:03:16 -0800

    CA-5541 PEP8: Improve spacing

--- dynsettings/admin/model_admins.py
+++ dynsettings/admin/model_admins.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from django.contrib import admin
+
 from dynsettings.models import BucketSetting
 from dynsettings.admin.views import edit_settings
 
@@ -9,37 +10,39 @@ class BucketSettingInline(admin.TabularInline):
 
 
 class SettingAdmin(admin.ModelAdmin):
-    
+
     # Changelist files
-    list_display = ('key','value', 'help_text','data_type')
+    list_display = ('key', 'value', 'help_text', 'data_type')
     list_editable = ('value',)
-    #linked_display_links = ()
-    
-    fields = ('key','value','data_type',)
-    readonly_fields = ('key','help_text','data_type')
+    # linked_display_links = ()
+
+    fields = ('key', 'value', 'data_type',)
+    readonly_fields = ('key', 'help_text', 'data_type')
     search_fields = ('key',)
     ordering = ('key',)
     actions = []
-    
+
     inlines = [BucketSettingInline]
-    
+
     def key(self, obj):
         return obj.key
-    
+
     def has_add_permission(self, request):
         return False
-    
+
     def get_urls(self):
         urls = super(SettingAdmin, self).get_urls()
-        my_urls = patterns('',
-            url(r'^edit/?$', self.admin_site.admin_view(edit_settings), name='dynsettings_setting_edit'),  
+        my_urls = patterns(
+            '',
+
+            url(r'^edit/?$', self.admin_site.admin_view(edit_settings), name='dynsettings_setting_edit'),
         )
         return my_urls + urls
-    
-    
+
+
 class BucketAdmin(admin.ModelAdmin):
-    list_display = ('key','bucket_type','probability','desc')
+    list_display = ('key', 'bucket_type', 'probability', 'desc')
     list_editable = ('probability',)
     ordering = ('key',)
-    
-    pass    
+
+    pass
