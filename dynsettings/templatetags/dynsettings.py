acf69c502dc3401f922277060705e076cc7f60f4
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-03-29 12:11:30 -0700

    CA-5840 PEP8: Improve spacing

--- dynsettings/templatetags/dynsettings.py
+++ dynsettings/templatetags/dynsettings.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
+
 from django import template
+
 from dynsettings.models import SettingCache
+
 
 register = template.Library()
 
@@ -8,4 +11,3 @@ register = template.Library()
 @register.assignment_tag
 def get_dynsetting(key, bucket=None):
     return SettingCache.valuedict[key](bucket)
-
