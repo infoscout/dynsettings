17662b7144f27ce7e00801f540f2f36a42bb86e8
Amy Sowash <sowashamy@gmail.com>
2018-05-16 11:57:35 -0700

    removed excess app_name in urls

--- dynsettings/tests/urls.py
+++ dynsettings/tests/urls.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 from django.conf.urls import url
 from django.contrib import admin
 
-app_name = 'dynsettings'
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
