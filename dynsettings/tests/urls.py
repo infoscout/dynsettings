7b2ec325b3bc82b3e15541ea5733e3a79e9a5c59
Phani Tekumalla <phani.tekumalla@numerator.com>
2024-04-30 09:47:40 -0400

    fix order per lint checks

--- dynsettings/tests/urls.py
+++ dynsettings/tests/urls.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.urls import include, re_path
 from django.contrib import admin
-
+from django.urls import include, re_path
 
 urlpatterns = [
     re_path(r'^admin/', include('admin.site.urls')),
