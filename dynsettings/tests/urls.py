db3a79217a51b2b966ef272d83f66f0aedba423f
Phani Tekumalla <phani.tekumalla@numerator.com>
2024-04-30 09:39:43 -0400

    django.conf.urls.url() was deprecated in Django 3.0

--- dynsettings/tests/urls.py
+++ dynsettings/tests/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf.urls import url
+from django.urls import include, re_path
 from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', include('admin.site.urls'),
 ]
